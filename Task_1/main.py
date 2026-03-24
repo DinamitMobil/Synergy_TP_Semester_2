2bc83b78c50fd4fea54b09b64b881d1c31e35cdf
DinamitMobil <i.ivanov-basic@yandex.ru>
2026-03-24 21:57:53 +0300

    Update main.py

--- Task_1/main.py
+++ Task_1/main.py
@@ -18,19 +18,48 @@ def find_sum_of_negatives_between_max_min(arr):
     return result_sum
 
 
+def entering_array_size():
+    # Обработка исключений ввода размера массива
+    while True:
+        try:
+            input_result = int(input("Введите размер массива: "))
+            if input_result > 1:
+                break
+            else:
+                print("Значение должно быть больше 1.")
+        except ValueError:
+            print("Значение может быть только целым числом.")
+    return input_result
+
+
+def entering_array_element(size, element_number):
+    # Обработка исключений ввода элементов массива
+    while True:
+        try:
+            input_result = int(
+                input(f"Элемент {element_number + 1} из {size} :"))
+            break
+        except ValueError:
+            print("Значение может быть только целым числом.")
+    return input_result
+
+
 # Пример использования
 if __name__ == "__main__":
-    # Ввод размера массива и его элементов
 
-    N = int(input("Введите размер массива: "))
-    A = []
+    # Ввод размера массива
+    size_array = entering_array_size()
+    elements = []
     print("Введите элементы массива.")
 
-    for i in range(N):
-        print(f"Элемент {i+1} из {N} :")
-        A.append(int(input()))
+    for i in range(size_array):
+        # Ввод элементов массива
+        elements.append(entering_array_element(size_array, i))
 
-    result = find_sum_of_negatives_between_max_min(A)
+    # Обработка полученного массива
+    result = find_sum_of_negatives_between_max_min(elements)
+
+    # Выводим результат
     print(
         f"Сумма отрицательных элементов между "
         f"максимальным и минимальным: {result}")
